2f358bd5722b28ff5599db515d0c54253be973ab
khanchandani-jai <khanchandanijai241@gmail.com>
2024-09-24 12:58:07 +0530

    chore: add bubble sort

---
+++ src/app/dsa/bubble-sort/page.js
@@ -0,0 +1,31 @@
+"use client";
+
+const BubbleSort = () => {
+  function bubbleSort(arr) {
+    let n = arr.length;
+    let swapped;
+
+    for (let i = 0; i < n - 1; i++) {
+      swapped = false;
+
+      for (let j = 0; j < n - i - 1; j++) {
+        console.log("hello");
+        if (arr[j] > arr[j + 1]) {
+          // Swap without using a temp variable
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+
+          swapped = true;
+        }
+      }
+      if (!swapped) {
+        break;
+      }
+    }
+    return arr;
+  }
+
+  console.log(bubbleSort([5, 1, 4, 2, 8]));
+  return <div>Bubble Sort</div>;
+};
+
+export default BubbleSort;
